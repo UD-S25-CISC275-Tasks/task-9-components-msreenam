d1ba68acf1208f9374b6730864f7fdb5afb10e59
Meera <meerasree05@gmail.com>
2025-03-07 12:01:56 -0500

    answer is revealed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "./App.css";
 //import { ChangeType } from "./components/ChangeType";
-//import { RevealAnswer } from "./components/RevealAnswer";
+import { RevealAnswer } from "./components/RevealAnswer";
 //import { StartAttempt } from "./components/StartAttempt";
 //import { TwoDice } from "./components/TwoDice";
 //import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
 
-// eslint-disable-next-line no-undef
-function App(): JSX.Element {
+function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
@@ -17,7 +16,7 @@ function App(): JSX.Element {
             <hr></hr>
             <Counter></Counter>
             <hr />
-            {/*<RevealAnswer></RevealAnswer>*/}
+            <RevealAnswer></RevealAnswer>
             <hr />
             {/*<StartAttempt></StartAttempt>*/}
             <hr />
